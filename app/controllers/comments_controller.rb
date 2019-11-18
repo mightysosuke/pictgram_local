645305ac94ce1b37a582d5d30e20791d5301b9f7
sosuke <sosuke.sin@gmail.com>
2019-11-18 11:07:36 +0900

    コメント機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,15 @@
 class CommentsController < ApplicationController
   def index
-    # code
+    # @topics = current_user.comment_topics
+    @topic = Topic.find_by(id: params[:topic_id])
   end
 
   def create
-    # code
+    comment = Comment.new
+    comment.user_id = current_user.id
+    comment.topic_id = params[:topic_id]
+    comment.comment_content = params[:comment_content]
+
+    redirect_to topics_path
   end
 end
